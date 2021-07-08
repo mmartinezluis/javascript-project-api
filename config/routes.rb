085dd93b02e2695af05344c2ba7e8011d1097f40
Luis Martinez <luis_mmartinez@live.com>
2021-07-07 23:44:04 -0400

    removed unneeded code from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,4 @@ Rails.application.routes.draw do
   resources :authors, only: [:index, :show]
   resources :quotes, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show] do 
-    resources :quotes, only: [:show]
-  end
-
 end
