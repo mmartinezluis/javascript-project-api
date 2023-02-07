42c77334e06412791aab4ae2d97b6a68ef426be2
Luis Martinez <luis_mmartinez@live.com>
2023-02-07 16:14:27 -0500

    add validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :stories, dependent: :destroy
   has_many :quotes, through: :stories
+  validates_presence_of :fist_name, :last_name, :email, :password, on: :cretae
+  validates :email, uniqueness: true, on: :create
+  before_validation { self.email = email.downcase }
 end
