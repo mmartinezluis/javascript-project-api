0378f7c2c8d950a9172714169a241d9848ad02f2
Luis Martinez <luis_mmartinez@live.com>
2023-02-09 18:46:46 -0500

    configure user profile action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
 
     def profile
-        
+        user = User.find_by(id: user_params[:id])
+        if user
+            render json: UserBlueprint.rneder(user, view: :profile)
+        else
+            render json: {message: "User not found"}, status: :not_found
+        end
+    end
+
+    private 
+    def user_params
+        params.require(:user).permit(:id)
     end
 end
