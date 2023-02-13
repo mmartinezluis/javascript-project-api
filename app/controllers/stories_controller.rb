932bca0cee304bb538b9c8dfde516acaef5ff3e0
Luis Martinez <luis_mmartinez@live.com>
2023-02-12 23:40:38 -0500

    add error boundaries to stories controller

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,7 +2,7 @@ class StoriesController < ApplicationController
   before_action :set_story, only: [:show, :update, :destroy]
   
   def index
-    stories = Story.all
+    stories = Story.includes(:quote).all
     render json: StoryBlueprint.render(stories, view: :normal)
   end
   
@@ -12,29 +12,37 @@ class StoriesController < ApplicationController
   
   def create
     story = Story.new(story_params)
-      if story.save
-      render json: StoryBlueprint.render(story, view: :normal)
+    if story.save
+      render json: StoryBlueprint.render(story, view: :normal), status: 200
     else
-      render json: @story.errors
+      render json: story.errors.full_messages.join("; "), status: :not_acceptable
     end
   end
 
   def update
-    if @story.update(story_params)
-      render json: StoryBlueprint.render(@story, view: :normal)
+    if !@story  
+      # render json: "Story not found", status: 404
+    elsif @story.id != story_params[:user_id]
+      render json: "Your are trying to update another user's story", status: :not_acceptable
+    elsif @story.update(story_params)
+      render json: StoryBlueprint.render(@story, view: :normal), status: 200
     else
-      render json: @story.errors, status: :unprocessable_entity
+      render json: @story.errors.full_messages, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @story.destroy
-    render json: {message: "Story ##{@story.id} successfully deleted"}
+    if @story.destroy
+      render json: {message: "Story ##{@story.id} successfully deleted"}
+    else 
+      render json: @story.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   private
     def set_story
-      @story = Story.find(params[:id])
+      @story = Story.find_by(id: params[:id])
+      render json: "Story not found", status: 404 and return if !@story
     end
 
     def story_params
